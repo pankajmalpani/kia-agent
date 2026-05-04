17536f1b01fbb30233491f3d7b14d0f4ba285dd9
Pankaj <psm041986@gmail.com>
2026-05-04 10:13:38 -0500

    fix api key reading

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from groq import Groq
 load_dotenv()
 
 app = Flask(__name__)
-client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+api_key = os.environ.get("GROQ_API_KEY")
+client = Groq(api_key=api_key)
 
 @app.route("/")
 def home():
